967bb3d73d29e2a571e470021a8cfd2f46042aa7
mary <immaculatekampire4@gmail.com>
2026-08-24 20:44:39 +0400

    added notes

--- analysis.py
+++ analysis.py
@@ -1,10 +1,2 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-pd.set_option("display.max_columns", None)
-pd.set_option("display.width", None)
-sns.set_style("whitegrid")
-df= pd.read_csv(r"basics/german_credit_data.csv")
-df.head()
+#1-Good aka Lower risk
+#0-Bad aka Higher risk
